7a955ef9154c4dc0d956d8ed94fd17c4f234a132
ArturUvarov <artur.uvarov@stud.ktmc.lt>
2025-02-17 12:30:55 +0200

    First version of docker connect

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`API server running on port ${port}`);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close(() => {
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received: closing HTTP server");
+  server.close(() => {
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working!" });
+});
