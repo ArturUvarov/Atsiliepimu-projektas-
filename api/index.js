192f072171eb35ba1e5082f6faf0f700fef47b1a
ArturUvarov <artur.uvarov@stud.ktmc.lt>
2025-02-17 14:18:02 +0200

    First api version

--- api/index.js
+++ api/index.js
@@ -10,14 +10,6 @@ const server = app.listen(port, () => {
   console.log(`API server running on port ${port}`);
 });
 
-process.on("SIGTERM", () => {
-  console.log("SIGTERM signal received: closing HTTP server");
-  server.close(() => {
-    console.log("HTTP server closed");
-    process.exit(0);
-  });
-});
-
 process.on("SIGINT", () => {
   console.log("SIGINT signal received: closing HTTP server");
   server.close(() => {
