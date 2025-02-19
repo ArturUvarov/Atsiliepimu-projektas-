193fb560aa7be32f975594920a15b5cf9269e7a2
NaglisKazlauskas <naglis.kazlauskas@ktmc.stud.lt>
2025-02-19 10:01:00 +0200

    ts

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "API is working!" });
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`API server running on port ${port}`);
+});
