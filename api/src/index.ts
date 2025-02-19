377ef5f1f20a77a7ceb6196fd3a7bc62cb13e93f
giedrius-ktmc <giedrius.stabingis@ktmc.lt>
2025-02-19 22:34:14 +0200

    pajungtas seeding

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,11 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { drizzle } from "drizzle-orm/mysql2";
+import { User } from "./db/schema";
+
+const db = drizzle({ connection: { uri: process.env.DATABASE_URL } });
+
 import cors from "cors";
 
 const app: Express = express();
@@ -15,6 +20,11 @@ app.get("/api/", (req: Request, res: Response) => {
 
 app.get("/api/test", (req: Request, res: Response) => {
   res.json({ message: "TEST!" });
+});
+
+app.get("/api/users", async (req: Request, res: Response) => {
+  let users = await db.select().from(User); 
+  res.json(users);
 });
 
 const port = process.env.PORT || 3000;
