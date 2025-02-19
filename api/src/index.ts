a01e563ee294b5ed4720c5727874ef0b636f157e
giedrius-ktmc <giedrius.stabingis@ktmc.lt>
2025-02-19 10:53:45 +0200

    stvarkytas nginx

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,11 +9,17 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api/", (req: Request, res: Response) => {
   res.json({ message: "API is working!" });
+});
+
+app.get("/api/test", (req: Request, res: Response) => {
+  res.json({ message: "TEST!" });
 });
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`API server running on port ${port}`);
 });
+
+
