abb3a03b5760169b036f7832f5bf25146fdd1cea
D0xszamb <dominykas.sambarys@stud.ktmc.lt>
2025-02-20 11:15:17 +0200

    commit

--- db/setup.sql
+++ db/setup.sql
@@ -2,7 +2,15 @@ CREATE DATABASE IF NOT EXISTS users;
 USE users;
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTCREATE TABLE `users_table` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`age` int NOT NULL,
+	`email` varchar(255) NOT NULL,
+	CONSTRAINT `users_table_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_table_email_unique` UNIQUE(`email`)
+);
+O_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE
 );
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
-  schema: "./db/schema.ts",
-  out: "./drizzle",
-  driver: "pg",
-  dbCredentials : {
-    connectionString: process.env.NEON_DATABASE_URL!,
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'mysql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
