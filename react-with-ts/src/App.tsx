f7873766f606e5465c12071de6a72f5856f29ea9
ArturUvarov <artur.uvarov@stud.ktmc.lt>
2025-02-25 22:02:25 +0200

    Update .gitignore

--- react-with-ts/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import Homepage from "./pages/Homepage";
-function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
-      <Homepage />
-    </div>
-  );
-}
-export default App;
