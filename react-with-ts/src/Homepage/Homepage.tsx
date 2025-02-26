69cfa277b5e34c32f915321493ac385290933409
ArturUvarov <artur.uvarov@stud.ktmc.lt>
2025-02-26 15:00:17 +0200

    Material Tailwind testing

--- react-with-ts/src/Homepage/Homepage.tsx
+++ react-with-ts/src/Homepage/Homepage.tsx
@@ -19,11 +19,12 @@ function Homepage() {
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           <div className="bg-white rounded-lg shadow-lg p-6">
             <a href="">Hello</a>
+            <div className="flex gap-4">
+            </div>
           </div>
         </div>
       </main>
     </div>
   );
 }
-
 export default Homepage;
