4f2e416b6ff8f68e0860190a531a55d8d234dc5f
Artur <artur.uvarov@stud.ktmc.lt>
2025-03-31 12:07:34 +0300

    test

--- react-with-ts/src/pages/user/user.jsx
+++ react-with-ts/src/pages/user/user.jsx
@@ -18,6 +18,8 @@ export function User() {
       {data.map((user) => (
         <div key={user.id}>
           <h1>{user.id}</h1>
+                  <h1>{user.email}</h1>
+
         </div>
       ))}
     </div>
