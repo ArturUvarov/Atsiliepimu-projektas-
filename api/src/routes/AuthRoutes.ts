8e4e87ecc35e92749ae5bcd8b1b649afae3d4745
NaglisKazlauskas <naglis.kazlauskas@ktmc.stud.lt>
2025-04-16 09:34:52 +0300

    added basic login code to authcontroller and authrouter

--- api/src/routes/AuthRoutes.ts
+++ api/src/routes/AuthRoutes.ts
@@ -8,4 +8,12 @@ router.post('/signup', (req, res, next) => {
     AuthController.register(req, res, next);
   });
 
+  router.post('/signin', async (req, res, next) => {
+    try {
+        await AuthController.login(req, res, next);
+    } catch (error) {
+        console.error("Error in /signin route:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
 export default router;
