e896b1088bc3f794d730faa0297aa37950b1905b
Artur <artur.uvarov@stud.ktmc.lt>
2025-05-06 15:12:42 +0300

    Optimizing project structure and added new design

--- react-with-ts/src/routes.jsx
+++ react-with-ts/src/routes.jsx
@@ -1,12 +1,14 @@
-import {
-  UserCircleIcon,
-  ServerStackIcon,
-  RectangleStackIcon,
-  ChatBubbleLeftEllipsisIcon,
-} from "@heroicons/react/24/solid";
 import { Profile } from "@/pages/dashboard";
 import { SignIn, SignUp } from "@/pages/auth";
 import { ChatGpt } from "@/pages/chatgpt";
+import { FeedMain } from "./pages/feed";
+import {
+  MegaphoneIcon,
+  UserCircleIcon,
+  ChatBubbleLeftEllipsisIcon,
+  UserPlusIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -16,6 +18,12 @@ export const routes = [
   {
     layout: "dashboard",
     pages: [
+      {
+        icon: <MegaphoneIcon {...icon} />,
+        name: "feed ",
+        path: "/feed",
+        element: <FeedMain />,
+      },
       {
         icon: <UserCircleIcon {...icon} />,
         name: "profile",
@@ -35,13 +43,13 @@ export const routes = [
     layout: "auth",
     pages: [
       {
-        icon: <ServerStackIcon {...icon} />,
+        icon: <UserGroupIcon {...icon} />,
         name: "sign in",
         path: "/sign-in",
         element: <SignIn />,
       },
       {
-        icon: <RectangleStackIcon {...icon} />,
+        icon: <UserPlusIcon {...icon} />,
         name: "sign up",
         path: "/sign-up",
         element: <SignUp />,
